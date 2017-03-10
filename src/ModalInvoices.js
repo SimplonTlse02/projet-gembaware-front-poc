469f72fc2dbc5688bee6cfc814cab82e3f10359f
Laure-Milian <laure.milian@gmail.com>
2017-03-10 14:04:48 +0100

    fix bug clic sur graphique

---
+++ src/ModalInvoices.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
+
+const ModalInvoices= () => (
+  <Modal trigger={<Button>Show Modal</Button>}>
+    <Modal.Header>Select a Photo</Modal.Header>
+    <Modal.Content image>
+      <Image wrapped size='medium' src='http://semantic-ui.com/images/avatar2/large/rachel.png' />
+      <Modal.Description>
+        <Header>Default Profile Image</Header>
+        <p>We've found the following gravatar image associated with your e-mail address.</p>
+        <p>Is it okay to use this photo?</p>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
+)
+
+export default ModalInvoices
